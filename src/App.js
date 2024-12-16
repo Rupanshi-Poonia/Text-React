766cd8378b05a57f1da5192cf79afa361e217d66
Rupanshi <rupanshi.worldref@gmail.com>
2024-12-16 14:14:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -22,12 +22,28 @@ export default function App() {
 
   const [mode, setMode] = useState('light');
 
-  const toggleMode = () => {
+  const removeclass = () => {
+    document.body.classList.remove("bg-primary")
+    document.body.classList.remove("bg-danger" )
+    document.body.classList.remove("bg-success" )
+    document.body.classList.remove("bg-warning" )
+    document.body.classList.remove("bg-light" )
+    document.body.classList.remove("bg-dark" )
+
+  }
+
+  const toggleMode = (cls) => {
+    removeclass();
+    console.log(cls)
+    document.body.classList.add("bg-" + cls)
+    
     if (mode === 'light') {
       setMode('dark');
+      document.body.style.backgroundColor = "#042743"
       showAlert('Dark mode has been enabled', 'success');
     } else {
       setMode('light');
+       document.body.style.backgroundColor = "white"
       showAlert('Light mode has been enabled', 'success');
     }
   };
